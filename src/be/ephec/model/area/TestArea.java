911c5c1d2b90e664ab734fbc4e8c683eb71adeff
jbeersaerts <jonathan.beersaerts1993@gmail.com>
2013-11-23 02:23:52 +0100

    test of the eclipse pull so modifying code on web gitHub

--- src/be/ephec/model/area/TestArea.java
+++ src/be/ephec/model/area/TestArea.java
@@ -8,7 +8,7 @@ public class TestArea {
 		Area are = new Area(10);
 		DeathStar death = new DeathStar();
 		System.out.println(death.isPlacable(are, 0, 0));
-		System.out.println(death.putPiece(are, 5,5 ));
+		System.out.println(death.putPiece(are, 7,5 ));
 		are.affiche();
 	}
 
